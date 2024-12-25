3d140255da6f5d98b43bc09f96219f5c0e5756a9
saenz-smct-26 <saenz.smct.26@gmail.com>
2024-12-25 14:52:15 -0600

    Clean up repository and apply .gitignore rules

---
+++ fun-stuff/calculator.js
@@ -0,0 +1,13 @@
+const display = document.getElementById('screen');
+
+function addToScreen(newValue) {
+  display.value += newValue;
+}
+
+function clearscreen() {
+  display.value ="";
+}
+
+function calculate() {
+  
+}
---
+++ practice.js
@@ -0,0 +1,35 @@
+/*let js = 'amazing';
+console.log(48 + 8 + 23 - 10);
+
+console.log("Isaac");
+console.log(23);
+
+let firstname = "Ivaan";
+let first = "Kelsey";
+let favorite_subject = "cosmology";
+let favorite_food = "Pizza";
+
+
+console.log(firstname);
+console.log(firstname);
+console.log(firstname);
+
+let three_years = 3;*/
+
+let earthisFlat = 'is_true';
+console.log(earthisFlat)
+
+console.log(typeof earthisFlat);
+console.log(typeof earthisFlat);
+console.log(typeof earthisFlat);
+console.log(typeof earthisFlat);
+let x = 20 + 10;
+x += 10;
+x *= 2;
+x /= 2;
+x ++;
+x --;
+console.log(x);
+
+const isfullAge = 24;
+console.log(isfullAge >= 25);
